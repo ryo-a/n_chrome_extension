9e0bcbd9de1d04cc0555c814ad2ad150e4bc8494
折原　ダビデ竜 <davideryu_orihara@daorihara.local>
2016-04-12 21:18:15 +0900

    my first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+$('.modal-window-inner').css('width', '100%');
+$('.modal-window-content').css('width', '100%');
+$('#modal-inner-iframe').css('width', '100%');
+$('#modal-inner-iframe').attr('src', $('#modal-inner-iframe').attr('src'));
