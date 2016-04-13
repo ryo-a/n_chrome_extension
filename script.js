d2de1cd20fd04cb29a2ddee037ccbc409939effc
折原　ダビデ竜 <davideryu_orihara@daorihara.local>
2016-04-13 18:31:01 +0900

    縦幅も height: 100% にするアップデート

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$('.modal-window-inner').css('width', '100%');
-$('.modal-window-content').css('width', '100%');
-$('#modal-inner-iframe').css('width', '100%');
+$('.modal-window-inner').css('width', '100%').css('height', '100%');
+$('.modal-window-content').css('width', '100%').css('height', '100%');
+$('#modal-inner-iframe').css('width', '100%').css('height', '100%');
 $('#modal-inner-iframe').attr('src', $('#modal-inner-iframe').attr('src'));
